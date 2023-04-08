0400a1e911452e97b685cd6c961090b49c8e2311
Ivad Yves HABIMANA <54445417+Yvad60@users.noreply.github.com>
2023-04-08 18:43:43 +0200

    Add custom styles

--- src/MySwiper.jsx
+++ src/MySwiper.jsx
@@ -1,9 +1,41 @@
+import { useEffect, useRef } from "react";
 import { register } from "swiper/element/bundle";
 register();
 
 const MySwiper = () => {
+  const swiperRef = useRef(null);
+
+  useEffect(() => {
+    const swiperContainer = swiperRef.current;
+    const params = {
+      navigation: true,
+      pagination: true,
+      // These are new...
+      injectStyles: [
+        `
+          .swiper-button-next,
+          .swiper-button-prev {
+            background-color: white;
+            padding: 8px 16px;
+            border-radius: 100%;
+            border: 2px solid black;
+            color: red;
+          }
+          .swiper-pagination-bullet{
+            width: 40px;
+            height: 40px;
+            background-color: red;
+          }
+      `,
+      ],
+    };
+
+    Object.assign(swiperContainer, params);
+    swiperContainer.initialize();
+  }, []);
+
   return (
-    <swiper-container navigation="true" pagination="true">
+    <swiper-container ref={swiperRef} init="false">
       <swiper-slide class="blue-slide">Slide 1</swiper-slide>
       <swiper-slide class="yellow-slide">Slide 2</swiper-slide>
       <swiper-slide class="green-slide">Slide 3</swiper-slide>
